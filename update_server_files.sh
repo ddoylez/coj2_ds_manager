339cec7b0c9d2612d928f01eb86ea3ead012b744
Dan Doyle <ddoyey@gmail.com>
2022-04-24 09:42:20 -0500

    added file update script

---
+++ update_server_files.sh
@@ -0,0 +1,9 @@
+source_dir="/home/rogototo/git-repos/"
+target_dir="/home/rogototo/servers/coj2/CoJ2/"
+
+cp ${source_dir}coj2/Data69.pak ${target_dir}Data69.pak
+cp ${source_dir}coj2/Speech69.pak ${target_dir}Speech69.pak
+cp -r ${source_dir}coj2_maps/data/mapsnet ${target_dir}Data/mapsnet
+cp -r ${source_dir}coj2_maps/data/creatures ${target_dir}Data/creatures
+cp -r ${source_dir}coj2_maps/data/characters ${target_dir}Data/characters
+
